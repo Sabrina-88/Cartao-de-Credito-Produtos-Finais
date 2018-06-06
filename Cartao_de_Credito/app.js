1c4b061a049caec51c9198446fc7aae4b0024fc6
Sabrina Andrade <sabrina.andrade.leite@gmail.com>
2018-06-06 18:28:16 -0300

    alterei as variaveis e versao quase pronta

--- Cartao_de_Credito/app.js
+++ Cartao_de_Credito/app.js
@@ -1,44 +1,43 @@
 // JavaScript Document
 //function cartao(meuArray){//4083952015263 
-
 var numeroCartao = prompt("digite o numero do cartao");
-//[4, 0, 8, 3, 9, 5, 2, 0, 1, 5, 2, 6, 3 ]
 var meuArray = numeroCartao.split('');//transformou cada digito em array
 meuArrayInvertido = meuArray.reverse();// inverte o array
 
-
 //Transformando array em inteiros
 function arrayInteiros(meuArrayInvertido){
-  for (var i in meuArrayInvertido ){
+  for (var i in meuArrayInvertido){
 meuArrayInvertido[i] = parseInt(meuArrayInvertido[i]);
   console.log(meuArrayInvertido);
   }
  return meuArrayInvertido;
 }
-
 arrayInteiros(meuArrayInvertido);
 
+// Separando pares, multiplicando e somando o resultado
+function separarMultiplicarSomar(meuArrayInvertido){
+  var resultForPair = 0;
+  var evenPosition = 0;
+  		for (i = 1; i < meuArrayInvertido.length; i++){// arrayImpar
+        
+    	  resultForPair = meuArrayInvertido[i]*2;
+        console.log(resultForPair);
 
-
-function parEimpar(arrayInteiros){
-for (var i = 0; i < meuArrayInvertido.length; i++){
-  if (i % 2 !== 0){
-    console.log("Indice é "+ [i] + ". E o valor é " + meuArrayInvertido[i]);
- }
-  var multiplicaDois = meuArrayInvertido[i] * 2;
-    console.log(meuArrayInvertido[i] + " x 2 = " + multiplicaDois);
-  if(multiplicaDois >9){
-   var numMaiorqueNove = multiplicaDois - 9;
-   console.log("- 9 = " + numMaiorqueNove);
+    		if(resultForPair > 9){
+      			//var moreThanNine = resultForPair - 9;
+            resultForPair = resultForPair - 9;
+            evenPosition = evenPosition + resultForPair;
+           //evenPosition = evenPosition + moreThanNine;
+            console.log(evenPosition);
+    		}else {
+      			evenPosition = evenPosition + resultForPair;
+            console.log (evenPosition);
+    		}
+    		i++;
+		}
+} for (i = 0; i < meuArrayInvertido.length; i++){// arrayPar
+        // var resultForOdd = 0;
+    	  // resultForOdd += resultForOdd;
+        // console.log(resultForOdd);
 }
-}
-
-//somar todos os multiplicaDois e depois somar os impares e depois somar todos para dar o valor total???
-// else (i % 2 === 0){ somar numeros entre si numa variavel soma impares}
-for (var i = 0; i < meuArrayInvertido.length; i++){
-    if(i % 2 === 0){
- console.log("Indice "+ i + ". E o valor é " + meuArrayInvertido[i]);
- }
- }
-}
-parEimpar();
+separarMultiplicarSomar(meuArrayInvertido);
